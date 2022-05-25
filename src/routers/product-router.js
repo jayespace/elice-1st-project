bb94b4c534fa1419416a802dfc73d3ead9f42ef7
[SW2/서울/1반] 신재이 <jshin0219@gmail.com>
2022-05-25 05:14:44 +0000

    2nd commit

--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -1,40 +1,71 @@
 import { Router } from 'express';
-// import is from '@sindresorhus/is';
+import is from '@sindresorhus/is';
 import { productService } from '../services';
+import asyncHandler from '../middlewares/async-handler';
 
 const productRouter = Router();
 
-productRouter.get('/products', async (req, res) => {
-    const products = await productService.getProducts();
+productRouter.get('/products', asyncHandler(async (req, res) => {
+    const totalProducts = await productService.countTotalProducts();
 
-    if (products.length < 1) {
-        res.json('no products');
-        return;
-    }
-    res.status(200).json(products);
-})
+    const page = Number(req.query.page || 1);
+    const perPage = Number(req.query.perPage || 10);
 
-productRouter.get('/products/:category', async (req, res) => {
+    const products = await productService.getProductByPages(page, perPage);
+
+    const totalPage = Math.ceil(totalProducts / perPage);
+
+    res.status(200).json({
+        products,
+        page,
+        perPage,
+        totalPage,
+        totalProducts
+        }
+    );
+}));
+
+productRouter.get('/products/category/:category', asyncHandler(async (req, res) => {
     const { category } = req.params;
-    const filtered = await productService.getProductsByCategory(category);
-    res.status(200).json(filtered);
-})
+    const totalProducts = await productService.countCategorized(category);
 
-productRouter.get('/products/:_id', async (req, res) => {
-    const { _id } = req.params;
-    const product = await productService.getProductDetail(_id);
+    const page = Number(req.query.page || 1);
+    const perPage = Number(req.query.perPage || 10);
+
+    const products = await productService.getProdByCatByPages(category, page, perPage);
+
+    const totalPage = Math.ceil(totalProducts / perPage);
+
+    res.status(200).json({
+        products,
+        page,
+        perPage,
+        totalPage,
+        totalProducts
+        }
+    );
+}));
+
+productRouter.get('/products/:productId', asyncHandler(async (req, res) => {
+    const { productId } = req.params;
+    const product = await productService.getProductDetail(productId);
     res.status(200).json(product);
-})
+}));
 
-productRouter.post('/products', async(req,res) => {
-    const { name, price, category, desc } = req.body;
-    const { admin } = req.query;
+productRouter.get('/categories', asyncHandler(async (req, res) => {
+    const categories = await productService.getCategories();
+    res.status(200).json(categories);
+}));
 
-    if(admin !== 'true') {
-        console.log('admin만 생성 가능합니다.');
-        res.redirect('/');
-        return;
+productRouter.post('/products', asyncHandler(async(req,res) => {
+    
+    if (is.emptyObject(req.body)) {
+        throw new Error(
+        'headers의 Content-Type을 application/json으로 설정해주세요'
+        );
     }
+
+    const { name, price, category, desc } = req.body;
 
     const newProduct = await productService.addProduct({
         name,
@@ -43,44 +74,28 @@ productRouter.post('/products', async(req,res) => {
         desc
     });
     res.status(200).json(newProduct);
-})
+}));
 
-productRouter.delete('/products/:_id', async (req, res) => {
-    const { _id } = req.params;
-    const { admin } = req.query;
+productRouter.delete('/products/:productId', asyncHandler(async (req, res) => {
+    const { productId } = req.params;
 
-    if(admin !== 'true') {
-        console.log('admin만 삭제 가능합니다.');
-        res.redirect('/');
-        return;
-    }
+    const del = await productService.deleteProduct(productId)
+    res.status(200).json(del);
 
-    await productService.deleteProduct(_id)
-    .then(result => {
-        return res.status(200).json('삭제가 완료되었습니다.');
-    }).catch(err => {
-        console.log(err);
-    });
+}));
 
 
-productRouter.patch('/products/:_id', async function (req, res, next) {
-    try {
-    // content-type 을 application/json 로 프론트에서
-    // 설정 안 하고 요청하면, body가 비어 있게 됨.
+productRouter.patch('/products/:productId', asyncHandler(async (req, res) => {
+
     if (is.emptyObject(req.body)) {
         throw new Error(
         'headers의 Content-Type을 application/json으로 설정해주세요'
         );
     }
 
-    const { _id } = req.params;
+    const { productId } = req.params;
     const { name, price, category, desc } = req.body;
 
-    const { admin } = req.query;
-
-    if (admin !== 'true') {
-        throw new Error('admin만 삭제 가능합니다.');
-    }
     // 위 데이터가 undefined가 아니라면, 즉, 프론트에서 업데이트를 위해
     // 보내주었다면, 업데이트용 객체에 삽입함.
     const toUpdate = {
@@ -89,16 +104,12 @@ productRouter.patch('/products/:_id', async function (req, res, next) {
         ...(category && { category }),
         ...(desc && { desc }),
     };
-    // 사용자 정보를 업데이트함.
-    const updatedProductInfo = await productService.setProduct(_id, toUpdate);
+    // 정보를 업데이트함.
+    const updatedProductInfo = await productService.setProduct(productId, toUpdate);
 
-    // 업데이트 이후의 유저 데이터를 프론트에 보내 줌
+    // 업데이트 이후의 데이터를 프론트에 보내 줌
     res.status(200).json(updatedProductInfo);
-    } catch (error) {
-    next(error);
-    }
-});
 
-});
+}));
 
 export { productRouter };
