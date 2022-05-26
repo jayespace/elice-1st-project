4c6770bea3b0be0bc872479ec6ab29ef5ed91bd9
gaeunn0724 <gaeunn0724@gmail.com>
2022-05-27 01:18:28 +0900

    list/detail page link

---
+++ src/views/product/product-detail.js
@@ -0,0 +1,10 @@
+const addBtn = document.querySelector(".add-button");
+const purchaseBtn = document.querySelector(".purchase-button");
+
+addBtn.addEventListener("click", () =>{
+    window.location.href = "cart.html";
+});
+
+purchaseBtn.addEventListener("click", () =>{
+    window.location.href = "cart.html";
+});
