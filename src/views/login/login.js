4cc0813ba44a7d73454cfdf5e3267ff5ce4d17ae
ouivve <ouivve@gmail.com>
2022-05-26 13:02:17 +0900

    로그인 성공시 토큰 및 유저정보를 세션 스토리지에 저장

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -40,10 +40,13 @@ async function handleSubmit(e) {
 
     const result = await Api.post('/api/login', data);
     const token = result.token;
+    const user = result.user;
+    const { fullName, role } = user;
 
-    // 로그인 성공, 토큰을 세션 스토리지에 저장
-    // 물론 다른 스토리지여도 됨
+    // 로그인 성공, 토큰 및 유저정보를 세션 스토리지에 저장
     sessionStorage.setItem('token', token);
+    sessionStorage.setItem('username', fullName);
+    sessionStorage.setItem('role', role);
 
     alert(`정상적으로 로그인되었습니다.`);
 
