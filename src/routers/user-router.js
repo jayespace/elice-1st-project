7ae0793cd28faddb0174dcf60f9672c5e7d2df33
[SW2/서울/1반] 박상준 <lovehp30@gmail.com>
2022-05-27 22:18:26 +0900

    user-router Merge 오류 제거

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -72,18 +72,6 @@ userRouter.post('/login', async function (req, res, next) {
 // 관리자 유저리스트를 가져옴 (배열 형태임) - 페이지네이션/검색기능
 // 미들웨어로 loginRequired 를 썼음 (이로써, jwt 토큰이 없으면 사용 불가한 라우팅이 됨)
 userRouter.get(
-<<<<<<< HEAD
-  '/userlist',
-  loginRequired,
-  adminRequired,
-  async function (req, res, next) {
-    try {
-      // 전체 사용자 목록을 얻음
-      const users = await userService.getUsers();
-
-      // 사용자 목록(배열)을 JSON 형태로 프론트에 보냄
-      res.status(200).json(users);
-=======
   '/admin/userlist',
   loginRequired,
   adminRequired,
@@ -209,7 +197,6 @@ userRouter.get(
 
       // 업데이트 이후의 유저 데이터를 프론트에 보내 줌
       res.status(200).json(findUserInfo);
->>>>>>> dev
     } catch (error) {
       next(error);
     }
