cdd338f87088f3bf9b8fdcae38205e289e95ac83
ouivve <ouivve@gmail.com>
2022-05-30 15:48:58 +0900

    페이지 접근시 최초 데이터 Read & Rendering, 장바구니 상품수 증가, 감소, 삭제 기능 구현

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -13,6 +13,7 @@ viewsRouter.use('/sellproduct', serveStatic('sellproduct'));
 viewsRouter.use('/editprofile', serveStatic('editprofile'));
 viewsRouter.use('/account', serveStatic('account'));
 viewsRouter.use('/products', serveStatic('product'));
+viewsRouter.use('/cart', serveStatic('cart'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
