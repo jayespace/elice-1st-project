b6bf41c9bb4d186d9f7d2fe51b867dcfc361d8b6
gaeunn0724 <gaeunn0724@gmail.com>
2022-05-31 03:42:00 +0900

    수량 변경

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -1,82 +1,102 @@
-import * as Api from '../api.js'; //api.js에서 쉽게 Post, Get을 가져올 수 있어요.
+import * as Api from '../api.js';
 
 const url = new URL(window.location.href);
 const id = url.searchParams.get("id");
 const detail = document.querySelector(".product-detail");
-console.log(id);
+const addBtn = document.querySelector('.add-button');
+const minusBtn = document.getElementById("minus");
+const plusBtn = document.getElementById("plus");
+const total = document.querySelector(".total");
+const qty = document.getElementById("qty");
 
-addAllElements(); //addAllElements 실행한데요
-addAllEvents(); //addAllEvents 도 실행합니다.
+let product = new Array(); //상품정보
+
+addAllElements();
+addAllEvents();
 getDataFromApi();
 
-const name = document.querySelector(".name");
-const price = document.querySelector(".price");
-const briefDesc = document.querySelector(".briefDesc");
-const fullDesc = document.querySelector(".fullDesc");
-const total = document.querySelector(".total");
-var value = document.querySelector('.select-qty');
-//     alert(value.options[value.selectedIndex].text);
-var get = new Array();
 async function getDataFromApi() {
-  // 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
-    const data = await Api.get('/api/products');
-    const arr = data.products;
+  const data = await Api.get('/api/products');
+  const arr = data.products;
+  product = arr.find(a => a._id === id);
 
-    get = arr.find(a=>a._id === id);
-
-    console.log(get.name);
-    insertHTMLToDetail(get);
-    name.innerText = get.name;
-    price.innerText = get.price +" 원";
-    briefDesc.innerText = get.briefDesc;
-    fullDesc.innerText = get.fullDesc;
-    total.innerText = get.price * Number(value.options[value.selectedIndex].text);
-    // price.innerText = get.name;
-
-//   addAllEvents();
+  insertHTMLToDetail(product);
+  insertValue();
 }
-addAllEvents();
+
 async function addAllElements() {
   
 }
 function addAllEvents() {
+  addBtn.addEventListener("click", addToCart);
+  minusBtn.addEventListener("click",minusQty);
+  plusBtn.addEventListener("click",plusQty);
 }
 
-const addbtn = document.querySelector('.buttons-container');
-addbtn.addEventListener("click",()=>{
-  get.count = 1; //상품 정보
-   
-    let getItem = JSON.parse(window.localStorage.getItem("cart"));
-    let objectId = get._id;
-
-    console.log(getItem);
-
-    if(getItem === null){
-        getItem = [];
-    } 
-    if(!getItem instanceof Array){
-      getItem = [getItem];
-      
-    }
-
-    getItem = getItem.filter(a=>a._id !== objectId);
-
-    getItem.push(get);
-    
-    window.localStorage.setItem("cart",JSON.stringify(getItem));
-  
-    console.log(getItem);
-})
 /**************여기 밑에 함수(function)을 입력해주세요***************/
-function insertHTMLToDetail(a){
+function insertHTMLToDetail(product) {
   detail.insertAdjacentHTML(
     'afterbegin',
     `
     <div class="product-image">
-        <img id="productImageTag" src="${a.image}" alt="상품이미지" />
+        <img id="productImageTag" src="${product.image}" alt="상품이미지" />
     </div>
     `
   );
 }
 
-//onchage함수....
+function insertValue(){
+  const name = document.querySelector(".name");
+  const price = document.querySelector(".price");
+  const briefDesc = document.querySelector(".briefDesc");
+  const fullDesc = document.querySelector(".fullDesc");
+  
+  name.innerText = product.name;
+  price.innerText = (product.price).toLocaleString('en') + " 원";
+  briefDesc.innerText = product.briefDesc;
+  fullDesc.innerText = product.fullDesc;
+  total.innerText = (product.price).toLocaleString('en') + " 원";
+}
+
+function addToCart() {
+  product.count = Number(qty.innerText); //상품 수량 추가
+
+  let getItem = JSON.parse(window.localStorage.getItem("cart"));
+  let objectId = product._id;
+
+  if(getItem === null) { //localstorage에 아무것도 없을 때
+    getItem = [];
+  }
+  if (!getItem instanceof Array) {
+    getItem = [getItem];
+  }
+  
+  getItem = getItem.filter(function(a){
+    if(a._id === objectId) alert('이미 장바구니에 담겨있어요!');
+    return a._id !== objectId;
+  });
+
+  getItem.push(product);
+
+  window.localStorage.setItem("cart", JSON.stringify(getItem));
+
+  console.log(getItem);
+}
+
+function minusQty() {
+  let num = Number(qty.innerText);
+  if(num > 1){
+    num -=1;
+  }
+  qty.innerText = num;
+  total.innerText = (product.price*num).toLocaleString('en') + " 원";
+}
+function plusQty() {
+  let num = Number(qty.innerText);
+    if(num < product.stock)
+      num +=1;
+    else alert('재고가 더 없어요(T_T)');
+
+    qty.innerText = num;
+    total.innerText = (product.price*num).toLocaleString('en') + " 원";
+}
--- src/views/product/product.js
+++ src/views/product/product.js
@@ -1,84 +1,41 @@
-import * as Api from '../api.js'; //api.js에서 쉽게 Post, Get을 가져올 수 있어요.
-// import { randomId } from '/useful-functions.js'; //useful-functions.js에서 기능 가져올 수 있다고 하네요.. 뭐 전 잘 안써요.
+import * as Api from '../api.js';
 
-// 요소(element), input 혹은 상수 <== 요기요기요기 요기 밑에 처럼 Node, 즉 안드로이드 findViewById 처럼 변수 - 태그 를 연결합니다.
-// const landingDiv = document.querySelector('#landingDiv');
-// const greetingDiv = document.querySelector('#greetingDiv');
 const list = document.querySelector(".product-list");
 
-// addAllElements(); //addAllElements 실행한데요
-// addAllEvents(); //addAllEvents 도 실행합니다.
 getDataFromApi();
+
 async function getDataFromApi() {
-  // 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
   const data = await Api.get('/api/products');
   const arr = data.products;
-  console.log(data.products);
-  console.log(data.products[0]._id);
 
-  arr.map((a)=>{
-      insertHTMLToList(a);
+  arr.map((product)=>{
+      insertHTMLToList(product);
   });
-
-  // addAllEvents();
 }
-addAllEvents();
-// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-async function addAllElements() {
-//   insertTextToLanding();
-//   insertTextToGreeting();
-}
-// const child = document.querySelector(".product-list").children[0];
-// 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-// 더 설명드리면 addEventListener 를 쓰는 문법? 문장 들을 넣어주면 될꺼에요
-function addAllEvents() {
-}
-
 
 /**************여기 밑에 함수(function)을 입력해주세요***************/
-function insertHTMLToList(a){
+function insertHTMLToList(product) {
   list.insertAdjacentHTML(
     'beforeend',
     `
-      <div class="product" id="${a._id}">
+      <div class="product" id="${product._id}">
         <div class="img">
-          <img src="${a.image}" alt="상품이미지">
+          <img src="${product.image}" alt="상품이미지">
         </div>
         <div class="content">
-          <h2 class="name" id="pname">${a.name}</h2>
-          <p class="price" id="pprice">${a.price}원</p>
+          <h2 class="name" id="pname">${product.name}</h2>
+          <p class="price" id="pprice">${product.price}원</p>
         </div>
       </div>
     `
   );
 
-  var url;
-  const id = document.getElementById(a._id);
-  // (url= `/detail?id=${a._id}`
- 
- 
-  id.querySelector('div').addEventListener('click',(url= `/detail?id=${a._id}` ,
-  function(){
-    // alert(a._id); //해당 id를 url에 붙여서 넘겨주고, 이를 받아오기...
-    window.location.href = url;
-  }
+  let url;
+  const id = document.getElementById(product._id);
+
+  id.querySelector('div').addEventListener('click', (url = `/detail?id=${product._id}`,
+    function () {
+      window.location.href = url;
+    }
   ))
- 
-}
-
-// function serialize(obj) {
-//   var str = [];
-//   for (var p in obj)
-//     if (obj.hasOwnProperty(p)) {
-//       str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-//     }
-//   return str.join("&");
-// }
-
-function alertLandingText() {
-  alert('n팀 쇼핑몰입니다. 안녕하세요.');
-}
-
-function alertGreetingText() {
-  alert('n팀 쇼핑몰에 오신 것을 환영합니다');
 }
