df81de592788e58e3deb200a8651916b9423893d
[SW2/서울/1반] 박상준 <lovehp30@gmail.com>
2022-05-30 19:31:10 +0900

    Views-router 정적주소 추가, account 수정, admin 관련 기능 생성

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -13,6 +13,8 @@ viewsRouter.use('/sellproduct', serveStatic('sellproduct'));
 viewsRouter.use('/editprofile', serveStatic('editprofile'));
 viewsRouter.use('/account', serveStatic('account'));
 viewsRouter.use('/products', serveStatic('product'));
+viewsRouter.use('/admin-userlist', serveStatic('admin-userlist'));
+
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
