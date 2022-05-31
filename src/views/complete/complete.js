e84fdc44a1eaec5076a03aa2d49df63b10301ca3
gaeunn0724 <gaeunn0724@gmail.com>
2022-06-01 03:59:37 +0900

    주문완료 화면

---
+++ src/views/complete/complete.js
@@ -0,0 +1,10 @@
+const orderBtn = document.getElementById("#orderDetailButton");
+const shopBtn = document.getElementById("#shoppingButton");
+
+orderBtn.addEventListener("click", ()=>{
+    window.location.href='';
+})
+
+orderBtn.addEventListener("click", ()=>{
+    window.location.href='';
+})
