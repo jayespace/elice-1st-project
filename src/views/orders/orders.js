87343b5facc72ab0b0dd3fcba761adccccf495b3
gaeunn0724 <gaeunn0724@gmail.com>
2022-06-01 22:26:47 +0900

    주문 정보조회 layout

--- src/views/orders/orders.js
+++ src/views/orders/orders.js
@@ -1,37 +1,57 @@
 import * as Api from "../api.js";
 import { addCommas } from "/useful-functions.js";
+const ordersTop = document.querySelector('.orders-top');
 
 getDataFromApi();
 
 async function getDataFromApi() {
   const data = await Api.get("/api/orders");
 
-  console.log(data);
+  console.log(data[0]);
+  console.log(data.length);
   
+  insertHTMLToList(data, data.length);
 }
 
-// function insertHTMLToList(product) {
-//   const { _id, image, name, price } = product;
-//   list.insertAdjacentHTML(
-//     "beforeend",
-//     `
-//       <div class="product" id="${_id}">
-//         <div class="img">
-//           <img src="${image}" alt="상품이미지">
-//         </div>
-//         <div class="content">
-//           <h2 class="name" id="pname">${name}</h2>
-//           <p class="price" id="pprice">${addCommas(price)}원</p>
-//         </div>
-//       </div>
-//     `
-//   );
+function insertHTMLToList(orderList,length) {
+    for(let i=0; i<length; i++){
+        const {orderInfo, productInfo, statusInfo, userInfo} = orderList[i];
+        console.log(orderInfo.createdAt);
+        const date = orderInfo.createdAt.split("T")[0];
+        const time = orderInfo.createdAt.split("T")[1].split(".")[0];
+    
+        let name = '';
+        let price = 0;
+        let qty = 0;
+        for(let i=0; i< productInfo.length; i++){
+            console.log(productInfo[i].name);
+            if(i === productInfo.length-1){
+              name += productInfo[i].name;
+            }else {
+              name += productInfo[i].name + `<br><br>`;
+            }
+            
+            price += productInfo[i].price;
+            qty += productInfo[i].qty;
+        }
+        
+      
+        console.log(name);
+        ordersTop.insertAdjacentHTML(
+            "afterend",
+            `
+            <div class="box colums orders-item">
+            <div class="date">${date} ${time}</div>
+            <div class="info">${name}</div>
+            <div class="qty">총 ${qty}개</div>
+            <div class="price">총 ${price.toLocaleString("en")}원</div>
+            <div class="status">${statusInfo.orderStatus}</div>
+            <div class="buttons">
+              <button class="order-detail">주문상세</button>
+              <button class="order-cancle">주문취소</button>
+            </div>
+          </div>
+            `
+    )}
 
-//   const id = document.getElementById(_id);
-//   function redirectProductDetail(e) {
-//     const url = `/detail?id=${_id}`;
-//     console.log(url);
-//     location.href = url;
-//   }
-//   id.addEventListener("click", redirectProductDetail);
-// }
+}
