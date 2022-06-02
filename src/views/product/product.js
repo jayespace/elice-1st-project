58314818e6da79c3818f82d76ec7a77c02da8eab
ouivve <ouivve@gmail.com>
2022-06-02 11:48:45 +0900

    세부 카테고리 경로 수정

--- src/views/product/product.js
+++ src/views/product/product.js
@@ -1,70 +1,72 @@
 /**
  * @todo
  * window.location.search 에따라 보여지는 products
- * 
+ *
  */
 
-import * as Api from "../api.js";
-import { addCommas } from "/useful-functions.js";
+import * as Api from '../api.js';
+import { addCommas } from '/useful-functions.js';
 
 const moreImage = document.getElementById('moreImage');
-const list = document.querySelector(".product-list");
+const list = document.querySelector('.product-list');
 globalThis.page = 1;
 globalThis.perPage = 8;
 const queryCategory = new URLSearchParams(location.search).get('category');
 console.log(queryCategory);
 //initial Create Products
-getDataFromApi(queryCategory,globalThis.page++, globalThis.perPage);
+getDataFromApi(queryCategory, globalThis.page++, globalThis.perPage);
 
 //infinity Scroll Function of Product Pagination
 //catch ScrollEvent
-const onScroll = e => {
-  const {scrollHeight, scrollTop, clientHeight } = e.target.scrollingElement
-  if(scrollHeight === scrollTop + clientHeight &&globalThis.page <=globalThis.totalPage){
+const onScroll = (e) => {
+  const { scrollHeight, scrollTop, clientHeight } = e.target.scrollingElement;
+  if (
+    scrollHeight === scrollTop + clientHeight &&
+    globalThis.page <= globalThis.totalPage
+  ) {
     getDataFromApi(queryCategory, globalThis.page++, globalThis.perPage);
   }
-}
+};
 //prevent repeating Call Function
 const debounce = (func, delay) => {
   let timeoutId = null;
   return (...args) => {
     clearTimeout(timeoutId);
-    timeoutId = setTimeout(func.bind(null, ...args ), delay);
-  }
-}
+    timeoutId = setTimeout(func.bind(null, ...args), delay);
+  };
+};
 
-document.addEventListener("scroll", debounce(onScroll, 150));
+document.addEventListener('scroll', debounce(onScroll, 150));
 
-function setMoreImage(page, totalPage){
-  if(page > totalPage){
+function setMoreImage(page, totalPage) {
+  if (page > totalPage) {
     moreImage.classList.add('is-hidden');
-  }else{
+  } else {
     moreImage.classList.remove('is-hidden');
   }
 }
 
 async function getDataFromApi(category, page, perPage) {
-  console.log('category',category);
-  const query =(category? `category='${category}'`:'')+
-  `&page=${page}&perPage=${perPage}`
-  try{  
-  const data = await Api.get("/api/products",query);
-  console.log(data);
-  const {totalPage, products} = data;
-  globalThis.totalPage = totalPage;
-  setMoreImage(globalThis.page, totalPage);
-  products.map(insertHTMLToList);
-
-  }catch(e){
-    console.error('페이지네이션 관련:',e);
+  console.log('category', category);
+  const query =
+    (category ? `category='${category}'` : '') +
+    `&page=${page}&perPage=${perPage}`;
+  try {
+    const data = await Api.get(`/api/products?${query}`);
+    console.log(data);
+    const { totalPage, products } = data;
+    globalThis.totalPage = totalPage;
+    setMoreImage(globalThis.page, totalPage);
+    products.map(insertHTMLToList);
+  } catch (e) {
+    console.error('페이지네이션 관련:', e);
   }
-
 }
 
 function insertHTMLToList(product) {
   const { _id, image, name, price } = product;
   list.insertAdjacentHTML(
-    "beforeend",
+    'beforeend',
     `
       <div class="product" id="${_id}">
         <div class="img">
@@ -84,5 +86,5 @@ function insertHTMLToList(product) {
     console.log(url);
     location.href = url;
   }
-  id.addEventListener("click", redirectProductDetail);
+  id.addEventListener('click', redirectProductDetail);
 }
