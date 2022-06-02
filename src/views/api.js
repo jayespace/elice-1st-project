ab8845c38a7f59395c2c7d7487114908c53f7aff
ouivve <ouivve@gmail.com>
2022-06-02 11:49:33 +0900

    get 요청 경로 수정

--- src/views/api.js
+++ src/views/api.js
@@ -1,6 +1,6 @@
 // api 로 GET 요청 (/endpoint/params 형태로 요청함)
 async function get(endpoint, params = '') {
-  const apiUrl = `${endpoint}?${params}`;
+  const apiUrl = `${endpoint}/${params}`;
   console.log(`%cGET 요청: ${apiUrl} `, 'color: #a25cd1;');
 
   const res = await fetch(apiUrl, {
@@ -40,7 +40,6 @@ async function post(endpoint, data) {
     },
     body: bodyData,
   });
-  
 
   // 응답 코드가 4XX 계열일 때 (400, 403 등)
   if (!res.ok) {
@@ -70,7 +69,6 @@ async function postMulti(endpoint, formData) {
     },
     body: formData,
   });
-  
 
   // 응답 코드가 4XX 계열일 때 (400, 403 등)
   if (!res.ok) {
@@ -84,7 +82,6 @@ async function postMulti(endpoint, formData) {
 
   return result;
 }
-
 
 // api 로 PATCH 요청 (/endpoint/params 로, JSON 데이터 형태로 요청함)
 async function patch(endpoint, params = '', data) {
@@ -150,4 +147,4 @@ async function del(endpoint, params = '', data = {}) {
 }
 
 // 아래처럼 export하면, import * as Api 로 할 시 Api.get, Api.post 등으로 쓸 수 있음.
-export { get, post, patch, del as delete ,postMulti};
+export { get, post, patch, del as delete, postMulti };
