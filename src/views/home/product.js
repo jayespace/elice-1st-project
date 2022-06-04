a41395822f3c7c3cfa11e1ad5d49437be1cfeba4
ouivve <ouivve@gmail.com>
2022-06-04 11:49:25 +0900

    home main 상품 둘러보기 추가

---
+++ src/views/home/product.js
@@ -0,0 +1,25 @@
+import * as Api from '../api.js';
+
+const list = document.querySelector('.product-list');
+const id = documtne.querySelector('.product');
+
+async function getProducts() {
+  const data = await Api.get('/api/products');
+  const products = data.products;
+
+  const productList = products
+    .map((item) => {
+      return `
+        <div class="product" id="${item._id}">
+            <div class="img">
+                <img src="${item.image}" alt="상품이미지">
+            </div>
+        </div>
+    `;
+    })
+    .slice(0, 8)
+    .join('');
+  list.insertAdjacentHTML('beforeend', productList);
+}
+
+getProducts();
